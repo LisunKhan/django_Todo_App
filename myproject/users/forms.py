d9e39bee9e2b14efec4d8b3795bd5153a87f7558
Lisun-bs23 <90611108+LisunKhan@users.noreply.github.com>
2025-06-20 16:25:15 +0600

    Revert "Add .gitignore and untrack previously ignored files"

--- myproject/users/forms.py
+++ myproject/users/forms.py
@@ -42,4 +42,4 @@ class CustomAuthenticationForm(AuthenticationForm):
 class TodoForm(forms.ModelForm):
     class Meta:
         model = TodoItem
-        fields = ['title', 'description', 'time_spent']
+        fields = ['title', 'description']
